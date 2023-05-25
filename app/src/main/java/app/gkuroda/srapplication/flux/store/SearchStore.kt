831aeaa4a81db4c7efcb51b1c65becb0f794de81
gkuroda <gkuroda@netprotections.co.jp>
2023-05-25 19:38:23 +0900

    rxのプラグインを削除

--- app/src/main/java/app/gkuroda/srapplication/flux/store/SearchStore.kt
+++ app/src/main/java/app/gkuroda/srapplication/flux/store/SearchStore.kt
@@ -4,11 +4,9 @@ import app.gkuroda.srapplication.flux.Dispatcher
 import app.gkuroda.srapplication.flux.action.search.SearchAction
 import app.gkuroda.srapplication.flux.api.SearchResponse
 import com.github.kittinunf.result.Result
-import com.orhanobut.logger.Logger
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.withContext
 
 interface SearchStore {
